dc61448fdd9126acaea85e16da3e52e2550fa2db
Rodolpho Reis <rodolphoreis@live.com>
2024-03-25 18:54:55 +0000

    feat: add header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Header from "./components/header/header";
 
 function App() {
   return (
     <>
       <div>
+        <Header />
         <h2>Olá</h2>
       </div>
     </>

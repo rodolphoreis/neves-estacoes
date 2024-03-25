53eac619977f18bb3452a664774ab9c908cac52b
Rodolpho Reis <rodolphoreis@live.com>
2024-03-25 18:55:19 +0000

    feat: add Slideshow components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import Header from "./components/header/header";
+import Slideshow from "./components/slideshow/Slideshow";
 
 function App() {
   return (
     <>
       <div>
         <Header />
+        <div className="slideshow-container">
+          <Slideshow />
+        </div>
         <h2>Olá</h2>
       </div>
     </>

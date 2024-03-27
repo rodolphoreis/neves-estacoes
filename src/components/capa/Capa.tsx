c4f0852b49ff6b18fdd0880be771a08730605417
Rodolpho Reis <rodolphoreis@live.com>
2024-03-27 19:14:34 +0000

    feat: create component

---
+++ src/components/capa/Capa.tsx
@@ -0,0 +1,21 @@
+import "./capa.css";
+
+const Capa = () => {
+  return (
+    <main className="main_capa">
+      <div className="capa_container">
+        <div className="overlay"></div>
+        <div className="texto_sobreposto">
+          <h1>Pegue o melhor brigadeiro</h1>
+          <p>
+            Transforme sua festa em uma explosão de sabores! Brigadeiros,
+            milkshakes e açaís de dar água na boca, diretamente na sua
+            comemoração. Experiencie o melhor da doçura conosco!"
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Capa;

02819b084e2d8795348cb0f1b55e6d31adf49e3f
Rodolpho Reis <rodolphoreis@live.com>
2024-03-31 18:35:20 +0100

    feat: create style

--- src/components/qualidade/Qualidade.tsx
+++ src/components/qualidade/Qualidade.tsx
@@ -2,43 +2,48 @@ import "./qualidade.css";
 
 const Qualidade = () => {
   return (
-    <div className="qualidade_container">
-      <img
-        src="/chocolate.png"
-        alt="Imagem de leite derramado"
-        className="imagem_qualidade_chocolate"
-      />
-      <h2>Nós nos preocupamos com você!</h2>
-      <p className="paragrafo1">
-        Nossa dedicação à qualidade é evidente em cada produto que oferecemos.
-        Desde a cuidadosa seleção do leite mais fresco até a elaboração dos mais
-        requintados chocolates e bombons artesanais, cada item é preparado com
-        meticulosidade para garantir a satisfação dos nossos clientes. Nosso
-        compromisso com a excelência permeia todos os aspectos do nosso
-        trabalho, refletindo-se não apenas na qualidade dos produtos, mas também
-        no atendimento personalizado que oferecemos.
-      </p>
-      <p className="paragrafo2">
-        Na Neves Estações, a busca pela perfeição é constante, e é isso que nos
-        permite proporcionar momentos de puro prazer e satisfação aos nossos
-        clientes. Com ingredientes da mais alta qualidade e processos de
-        produção rigorosos, estamos comprometidos em oferecer produtos que não
-        apenas deleitam o paladar, mas também garantem a segurança e o bem-estar
-        dos nossos consumidores. Cada item é preparado meticulosamente, desde a
-        seleção cuidadosa do leite fresco até a elaboração dos mais requintados
-        chocolates e bombons artesanais, para satisfazer nossos clientes.
-      </p>
-      <img
-        src="/morango.png"
-        alt="Imagem de morango"
-        className="imagem_qualidade_morango2"
-      />
-      <img
-        src="/morango.png"
-        alt="Imagem de morango"
-        className="imagem_qualidade_morango"
-      />
-    </div>
+    <section className="section_Qualidade">
+      <div className="qualidade_container">
+        <img
+          src="/chocolate.png"
+          alt="Imagem de leite derramado"
+          className="imagem_qualidade_chocolate"
+        />
+        <h2>Nós nos preocupamos com você!</h2>
+        <p className="paragrafo1">
+          Nossa dedicação à qualidade é evidente em cada produto que oferecemos.
+          Desde a cuidadosa seleção do leite mais fresco até a elaboração dos
+          mais requintados chocolates e bombons artesanais, cada item é
+          preparado com meticulosidade para garantir a satisfação dos nossos
+          clientes. Nosso compromisso com a excelência permeia todos os aspectos
+          do nosso trabalho, refletindo-se não apenas na qualidade dos produtos,
+          mas também no atendimento personalizado que oferecemos.
+        </p>
+        <p className="paragrafo2">
+          Na Neves Estações, a busca pela perfeição é constante, e é isso que
+          nos permite proporcionar momentos de puro prazer e satisfação aos
+          nossos clientes. Com ingredientes da mais alta qualidade e processos
+          de produção rigorosos, estamos comprometidos em oferecer produtos que
+          não apenas deleitam o paladar, mas também garantem a segurança e o
+          bem-estar dos nossos consumidores. Cada item é preparado
+          meticulosamente, desde a seleção cuidadosa do leite fresco até a
+          elaboração dos mais requintados chocolates e bombons artesanais, para
+          satisfazer nossos clientes.
+        </p>
+        <div className="morangos">
+          <img
+            src="/morango.png"
+            alt="Imagem de morango"
+            className="imagem_qualidade_morango2"
+          />
+          <img
+            src="/morango.png"
+            alt="Imagem de morango"
+            className="imagem_qualidade_morango"
+          />
+        </div>
+      </div>
+    </section>
   );
 };
 

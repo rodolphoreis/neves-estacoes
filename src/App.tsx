431cc003ce1520bad02398b73811ed8f39924d18
Rodolpho Reis <rodolphoreis@live.com>
2024-04-01 12:23:32 +0100

    feat: add component

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,18 @@ import "./App.css";
 import Capa from "./components/capa/Capa";
 import Header from "./components/header/header";
 import Qualidade from "./components/qualidade/Qualidade";
+import Slideshow from "./components/slideshow/Slideshow";
 import SobreNeves from "./components/sobre/SobreNeves";
-/* import Slideshow from "./components/slideshow/Slideshow"; */
 
 function App() {
   return (
     <>
       <div className="container">
         <Header />
-        {/* <div className="slideshow-container">
-          <Slideshow />
-        </div> */}
         <Capa />
         <SobreNeves />
         <Qualidade />
+        <Slideshow />
       </div>
     </>
   );
